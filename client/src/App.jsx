ee31ae53344dc4b778972bb6925aaee805623fce
Mateis <mateis.pro@outlook.fr>
2023-04-19 14:14:15 +0200

    v1.0.0

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import { ChannelContainer, ChannelListContainer } from "./components";
+
+const apiKey = 'xfuyqa2fp5e';
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
     return (
-        <div>
-            <h1>RT Chat Application</h1>
+        <div className="app__wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelListContainer/>
+                <ChannelContainer/>
+            </Chat>
         </div>
     );
 }
