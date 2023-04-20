bd3a3b03c3de6c335c582fdb014db0f8c5bc4f7c
Mateis <mateis.pro@outlook.fr>
2023-04-20 11:23:26 +0200

    v1.0.0

--- client/src/components/TeamChannelList.jsx
+++ client/src/components/TeamChannelList.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
 
-const TeamChannelList = () => {
-  return (
-    <div>TeamChannelList</div>
-  )
+import { AddChannel } from '../assets';
+
+const TeamChannelList = ({ children, error = false, loading, type }) => {
+    if (error) {
+        return type === "team" ? (
+            <div className='team-channel-list'>
+                <p className='team-channel-list__message'>
+                    Connection error, please wait a moment and try again.
+                </p>
+            </div>
+        ) : null;
+    }
+
+    if (loading) {
+        <div className='team-channel-list'>
+            <p className='team-channel-list__message'>
+                { type === "team" ? 'Channels' : "Messages" } loading...
+            </p>
+    </div>
+    }
+    
+    return (
+        <div className="team-channel-list">
+            <div className="team-channel-list__header">
+                <p className="team-channel-list__header__title">
+                    { type === "team" ? 'Channels' : "Direct Messages" }
+                </p>
+                {/* Button - add channel */}
+            </div>
+            
+            { children }
+        </div>
+    );
 }
 
-export default TeamChannelList
+export default TeamChannelList;
